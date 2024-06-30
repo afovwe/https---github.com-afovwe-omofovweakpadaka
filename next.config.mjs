25b1d05391a0cf160e8453c48b3f632f5fa9a517
Omofovwe Akpadaka <afovwe@gmail.com>
2024-06-30 07:12:38 -0700

    output add to config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 import {withSentryConfig} from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'standalone'
+};
 
 export default withSentryConfig(nextConfig, {
 // For all available options, see:
